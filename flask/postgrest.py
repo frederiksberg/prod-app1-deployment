ba16341afadee044b7a08d8d724ff97064473351
baffioso <baffyx@gmail.com>
2019-05-27 21:34:23 +0200

    modified metadata for swagger

--- flask/postgrest.py
+++ flask/postgrest.py
@@ -1,10 +1,42 @@
 import requests
+import json
 
 def metadata():
+    """
+    Manipulating OpenAPI definition from PostgREST
+    """
     url = "http://postgrest:3000"
     r = requests.get(url)
 
-    return r.text
+    meta = r.json()
+
+    # Remove /rpc/ and only keep GET
+    paths = {o_k: {i_k:i_v for (i_k, i_v) in o_v.items() if i_k == 'get'} for (o_k, o_v) in meta['paths'].items() if o_k[:5] != '/rpc/'}
+    # Only srid parameter
+    for k,v in paths.items():
+        if k != '/':
+            paths[k]['get']['parameters'] = [{ "$ref": "#/parameters/srid" }]
+
+    # Update meta file
+    meta['paths'] = paths
+
+    meta['parameters'] = {
+        "srid" : {
+            "name": "srid",
+            "required": False,
+            "in": "query",
+            "type": "integer"
+        }
+    }
+
+    # Chanage info
+    meta['info'] = {
+        "version": "v1",
+        "title": "Frederiksberg Kommunes API",
+        "description": "Denne side en automatisk genereret af PostgREST og efterfølgende tilrettet"
+    }
+
+    return json.dumps(meta)
 
 def to_geojson(table, srid=4326, where=None):
     url = "http://postgrest:3000/rpc/to_geojson"
