2fa80b02ccd7c7029adb3418061825c1bc68306f
Nicolaj Filrup Rasmussen <nixen97@gmail.com>
2019-06-21 13:47:02 +0200

    Env files for grafana

--- proxy/init.sh
+++ proxy/init.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-DOMAINS="grafana.frb-data.dk,monitor.frb-data.dk,nodered.frb-data.dk,th.frb-data.dk"
-
-docker exec reverse_proxy certbot --nginx -d grafana.frb-data.dk -d monitor.frb-data.dk -d nodered.frb-data.dk -d th.frb-data.dk -n --agree-tos --no-eff-email -m gis@frederiksberg.dk --staging
+docker exec reverse_proxy certbot --nginx -d grafana.frb-data.dk -d monitor.frb-data.dk -d nodered.frb-data.dk -d th.frb-data.dk -n --agree-tos --no-eff-email -m gis@frederiksberg.dk
 
 docker exec reverse_proxy sh -c 'echo ''certbot renew --post-hook reload nginx'' > /etc/cron.daily'
