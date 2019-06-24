f4dd1577e292565ad860143cd2367a2ddaffd195
Nicolaj Filrup Rasmussen <nixen97@gmail.com>
2019-06-24 13:34:46 +0200

    http to https redirect

--- proxy/init.sh
+++ proxy/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker exec reverse_proxy certbot --nginx -d grafana.frb-data.dk -d monitor.frb-data.dk -d nodered.frb-data.dk -d th.frb-data.dk -d api.frb-data.dk -nq --agree-tos --no-eff-email -m gis@frederiksberg.dk
+docker exec reverse_proxy certbot --nginx -d grafana.frb-data.dk -d monitor.frb-data.dk -d nodered.frb-data.dk -d th.frb-data.dk -d api.frb-data.dk -nq --agree-tos --redirect --no-eff-email -m gis@frederiksberg.dk
 
 docker exec reverse_proxy sh -c 'echo ''certbot renew --post-hook reload nginx'' > /etc/cron.daily'
