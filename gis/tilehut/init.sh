1863cf066e40f1c8cf3677d6f7482fab279f03af
Nicolaj Filrup Rasmussen <nixen97@gmail.com>
2019-06-24 14:34:18 +0200

    Changed permissions on sftp host keys

--- gis/tilehut/init.sh
+++ gis/tilehut/init.sh
@@ -1,3 +1,6 @@
 mkdir -p host_keys
 ssh-keygen -t ed25519 -f host_keys/ssh_host_ed25519_key -N '' < /dev/null 2>&1 >/dev/null
 ssh-keygen -t rsa -b 4096 -f host_keys/ssh_host_rsa_key -N '' < /dev/null 2>&1 >/dev/null
+
+chmod 400 ./host_keys/ssh_host_ed25519_key
+chmod 400 ./host_keys/ssh_host_rsa_key
