1e685375bee5ea5592b01a99f1793f50ed8a08fc
Nicolaj Filrup Rasmussen <nixen97@gmail.com>
2019-06-25 16:27:47 +0200

    Not actually needed. Handled by container

--- proxy/init.sh
+++ proxy/init.sh
@@ -2,4 +2,4 @@
 
 docker exec reverse_proxy certbot --nginx -d grafana.frb-data.dk -d monitor.frb-data.dk -d nodered.frb-data.dk -d th.frb-data.dk -d api.frb-data.dk -nq --agree-tos --redirect --no-eff-email -m gis@frederiksberg.dk
 
-docker exec reverse_proxy sh -c 'echo ''certbot renew --post-hook reload nginx'' > /etc/cron.daily'
+# docker exec reverse_proxy sh -c 'echo ''certbot renew --post-hook reload nginx'' > /etc/cron.daily'
