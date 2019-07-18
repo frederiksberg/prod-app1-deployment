25380e01ed436b039908e367e9556c78e7d46d80
Nicolaj Filrup Rasmussen <nixen97@gmail.com>
2019-07-18 12:48:28 +0200

    ACME v2

--- proxy/init.sh
+++ proxy/init.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker exec reverse_proxy certbot --nginx -d frb-data.dk -d *.frb-data.dk -nq --agree-tos --redirect --expand --no-eff-email -m gis@frederiksberg.dk
+docker exec reverse_proxy certbot --nginx -d frb-data.dk -d *.frb-data.dk -nq --agree-tos --redirect --expand --server https://acme-v02.api.letsencrypt.org/directory --no-eff-email -m gis@frederiksberg.dk
