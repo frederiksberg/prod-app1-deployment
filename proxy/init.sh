87de76111b82f8ad54a67817e1d9730fecd66935
Nicolaj Filrup Rasmussen <nixen97@gmail.com>
2019-07-18 12:43:21 +0200

    certbot wildcard

--- proxy/init.sh
+++ proxy/init.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-
-docker exec reverse_proxy certbot --nginx -d grafana.frb-data.dk -d monitor.frb-data.dk -d nodered.frb-data.dk -d th.frb-data.dk -d api.frb-data.dk -d tegola.frb-data.dk -d maputnik.frb-data.dk -d forecast.frb-data.dk -d frb-data.dk -nq --agree-tos --redirect --expand --no-eff-email -m gis@frederiksberg.dk
-
-# docker exec reverse_proxy sh -c 'echo ''certbot renew --post-hook reload nginx'' > /etc/cron.daily'
+docker exec reverse_proxy certbot --nginx -d *.frb-data.dk -nq --agree-tos --redirect --expand --no-eff-email -m gis@frederiksberg.dk
