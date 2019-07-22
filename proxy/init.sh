d5bd8408755d2441016c9e2ac468cc5ab9bca5f0
Nicolaj Filrup Rasmussen <nixen97@gmail.com>
2019-07-22 12:49:37 +0200

    Added VerjAPI source

--- proxy/init.sh
+++ proxy/init.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-# Keep this for now
 docker exec reverse_proxy certbot --nginx -d grafana.frb-data.dk -d monitor.frb-data.dk -d nodered.frb-data.dk -d th.frb-data.dk -d api.frb-data.dk -d tegola.frb-data.dk -d maputnik.frb-data.dk -d forecast.frb-data.dk -d frb-data.dk -nq --agree-tos --redirect --expand --no-eff-email -m gis@frederiksberg.dk
-
-# docker exec reverse_proxy certbot --authenticator standalone --installer nginx -d frb-data.dk -d *.frb-data.dk -nq --agree-tos --redirect --expand --server https://acme-v02.api.letsencrypt.org/directory --no-eff-email -m gis@frederiksberg.dk
