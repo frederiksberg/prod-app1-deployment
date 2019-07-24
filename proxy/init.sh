581fad3d606d730270d77cef3fd6d914ec2becdd
Nicolaj Filrup Rasmussen <nixen97@gmail.com>
2019-07-24 13:45:11 +0200

    Bug in urllib3 + 4096 bit RSA + better logging
    * Upgraded to different urllib3 version because of bug, when getting
    certs.
    
    * Upgraded to using a 4096 bit RSA for our cert
    
    * Properly configured nginx to output access.log and error.log to stdout
    and stderr respectively. Makes for nice logging output in
    docker/docker-compose.

--- proxy/init.sh
+++ proxy/init.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker exec reverse_proxy certbot --nginx -d frb-data.dk -d vejr.frb-data.dk -d grafana.frb-data.dk -d monitor.frb-data.dk -d nodered.frb-data.dk -d th.frb-data.dk -d api.frb-data.dk -d tegola.frb-data.dk -d maputnik.frb-data.dk -d forecast.frb-data.dk -nq --agree-tos --redirect --expand --no-eff-email -m gis@frederiksberg.dk
+docker exec reverse_proxy certbot --nginx \
+    -d frb-data.dk -d vejr.frb-data.dk -d grafana.frb-data.dk \
+    -d monitor.frb-data.dk -d nodered.frb-data.dk -d th.frb-data.dk \
+    -d api.frb-data.dk -d tegola.frb-data.dk -d maputnik.frb-data.dk \
+    -d forecast.frb-data.dk \
+    -nq --agree-tos --redirect --expand \
+    --no-eff-email -m gis@frederiksberg.dk \
+    --rsa-key-size=4096
