5a700baa75761b99af55af94cd9f8382899f6041
Nicolaj Filrup Rasmussen <nixen97@gmail.com>
2019-09-17 11:27:10 +0200

    Revert "UTD W/ production"
    This reverts commit 59e659a8c4ea72cbcc584f76ff785197ffe6f9a2.

--- proxy-dev/nginx/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -f "/etc/ssl/certs/dhparam.pem" ]; then
-    /usr/bin/openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
-    echo "Generated Diffie-Helmman key"
-fi
-
-nginx -g "daemon off;"
