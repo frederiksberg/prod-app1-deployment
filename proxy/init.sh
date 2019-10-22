935dd9adddd87eb1c2f24dc7631c662ff78370b6
nixen97 <37103694+nixen97@users.noreply.github.com>
2019-10-22 16:26:46 +0200

    Update init.sh

--- proxy/init.sh
+++ proxy/init.sh
@@ -3,7 +3,7 @@
 # SSL/TLS
 docker exec reverse_proxy certbot --nginx \
     -d frb-data.dk -d vejr.frb-data.dk -d grafana.frb-data.dk \
-    -d monitor.frb-data.dk -d nodered.frb-data.dk -d \
+    -d monitor.frb-data.dk -d nodered.frb-data.dk \
     -d th.frb-data.dk -d th1.frb-data.dk -d th2.frb-data.dk -d th3.frb-data.dk \
     -d api.frb-data.dk -d tegola.frb-data.dk -d maputnik.frb-data.dk \
     -d forecast.frb-data.dk -d cvr.frb-data.dk \
